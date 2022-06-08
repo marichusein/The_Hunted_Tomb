11e8a50dd03d39a413dfbb336aee335d03c2c849
a_sahovic565@hotmail.com <a_sahovic565@hotmail.com>
2022-06-08 16:33:27 +0200

    Shop first part

---
+++ Assets/Script/buttonInfo.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using  UnityEngine.UI;
+
+public class buttonInfo : MonoBehaviour
+{
+    public int itemID;
+    public Text priceText;
+    public GameObject shopManager;
+   
+    void Start()
+    {
+        
+    }
+    
+}
